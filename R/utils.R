05de17cc92cf56762955e574229d76066ddfc973
Korsunskiy <ilya.korsunsky@gmail.com>
2022-11-10 13:36:08 -0500

    added utils file for dplyr pipe operator

---
+++ R/utils.R
@@ -0,0 +1,14 @@
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @examples
+#' x <- 5 %>% sum(10)
+#' 
+#' @usage lhs \%>\% rhs
+#' @return return value of rhs function. 
+NULL
+
