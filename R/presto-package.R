c1e3eb6f1a3180518dbc6a3a68aff4bb27114fbc
Kamil Slowikowski <kslowikowski@gmail.com>
2026-08-25 19:09:18 -0400

    Import stats::rpois for the toy data generators

--- R/presto-package.R
+++ R/presto-package.R
@@ -6,7 +6,7 @@
 #' @importClassesFrom Matrix dgCMatrix dgTMatrix dgeMatrix TsparseMatrix
 #' @importFrom methods as is
 #' @importFrom Matrix Matrix
-#' @importFrom stats p.adjust pnorm wilcox.test
+#' @importFrom stats p.adjust pnorm rpois wilcox.test
 #' @importFrom utils head
 #' @importFrom Rcpp evalCpp sourceCpp loadModule
 #' @importFrom rlang .data
